0a637518540dad5f777a5a581ef6a98ea7ae632f
SiddhantBhardwaj2018 <bhardwajsiddhant03@gmail.com>
2020-10-24 13:48:17 -0700

    Updates

---
+++ precache-manifest.4e8d2cf94514f70865d67f8a57cf08b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8902b7d9558ffc3c22b8a6ffd13f03d2",
+    "url": "/Expense-Tracker-App-React/index.html"
+  },
+  {
+    "revision": "184b834ebeaf08943fb2",
+    "url": "/Expense-Tracker-App-React/static/css/main.bccd0344.chunk.css"
+  },
+  {
+    "revision": "116210585c6f08f97387",
+    "url": "/Expense-Tracker-App-React/static/js/2.ac98fd27.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Expense-Tracker-App-React/static/js/2.ac98fd27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "184b834ebeaf08943fb2",
+    "url": "/Expense-Tracker-App-React/static/js/main.0604923b.chunk.js"
+  },
+  {
+    "revision": "069cfb5efeb359a3d7a0",
+    "url": "/Expense-Tracker-App-React/static/js/runtime-main.555b338c.js"
+  }
+]);
